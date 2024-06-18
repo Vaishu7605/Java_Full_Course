9ebc014e8751da40144d7121773393834ff3bd06
Vaishnavi Kesherwani <122589971+Vaishu7605@users.noreply.github.com>
2024-06-18 16:37:55 +0530

    f

---
+++ loop.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class loop {
+
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter any Number: ");
+        Scanner s = new Scanner(System.in);
+        num = s.nextInt();
+        while (num >= 0) {
+            if (num % 2 == 0) {
+                System.out.println("Even Number");
+                break;
+            } else {
+                System.out.println("Odd Number");
+                break;
+            }
+        }
+        System.out.println("loop ended");
+    }
+}
